b373933635665088fff14197c3cdbf1fb65a8b8c
Boris <AlabamaBleach@gmail.com>
2021-09-28 12:03:42 +0300

    FirstInit

---
+++ Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitOfWork.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Phone { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
